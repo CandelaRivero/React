1c49f28f5c9750a80aa28e8cf7a4bda021b192a5
Candela Rivero <candelarivero01@gmail.com>
2022-02-13 17:08:10 -0500

    Tercer commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 function App() {
+
+ const mensajeBienvenida = 'Bienvenidos a React'
+
+ const stylesLi = {
+   color: 'red'
+ }
+
+ const alertMsg = () => { alert ("Hola") }
+
   return (
+    <>
+    <NavBar></NavBar>
     <div className="App">
-      <h1>HOLA MUNDO!</h1>
+      <h1 className="text-3xl font-bold underline">{mensajeBienvenida}</h1>
+      <ul>
+        <li style = {stylesLi}>React</li>
+        <li>JSX</li>
+        <li>Webpack</li>
+      </ul>
+      <Footer name="Maria" action={alertMsg}> 
+      
+      <h3>Mi children</h3>
+      <h4>otro children</h4>
+      </Footer>
     </div>
+    </>
+
+    
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+//import React from 'react'
+//snmipet: rafce
+
+//const footer = () => {
+ // return (
+   // <div>footer</div>
+  //)
+//}
+
+//export default footer
+
+function Footer ( {name, action, children} ) {     // si le pongo el action sale el artMsg, si dejo name toma el onclick
+
+//  const onClickHandler = () => { 
+  //  console.log('Hiciste click')
+  // }
+
+const styles = {
+  button: 'class=h-10 px-6 font-semibold rounded-md bg-black text-white'
+}
+    return (
+
+      <>
+      
+         {children.map ( e =>
+          <div>{e}</div>
+          )} 
+        <span>este es mi pie de pagina</span>
+        <div>Espero que te haya gustado mi sitio {name}</div>
+        <button className={styles.button} onClick={action} >Dale click aqui</button> 
+        
+        
+        
+      </>    
+// si le pong action salta el aler, si dejo la funcion toma el consolelog
+    )
+}
+
+export default Footer
+
