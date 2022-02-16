61e912cd75872f693ad86b09c3521a1001c41a22
Candela Rivero <candelarivero01@gmail.com>
2022-02-16 15:51:44 -0500

    Commit Items

---
+++ src/container/Item.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Item = ({producto}) => {
+  return (
+    <div>
+        <h3>{producto.nombre}</h3>
+        <h4>{producto.precio}</h4>
+    </div>
+  )
+}
+
+export default Item
--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -1,25 +1,33 @@
 
 
 import React from 'react'
+import Item from './Item'
 
 
 
 
-function ItemListContainer ( {nombre} ) {    
+function ItemListContainer ( {nombre}  ) {    
 
     const estilos = {
         nombre: 'class=h-10 px-6 text-center underline decoration-pink-500'
       }
+
+      const producto = [
+        {nombre: 'Cessna 152', precio: '$1500'},
+        {nombre: 'Cessna 172', precio: '$3000'},
+        {nombre: 'Piper Seneca', precio: '$6000'}
+      ]
     
         return (
-    
-          <>
-          
-    <div className={estilos.nombre} >Hola buen dia {nombre}!</div>
-  
-            
-            
-            
+          <> 
+          <div className={estilos.nombre} >Hola buen dia {nombre}!</div>  
+
+          <div className={estilos.nombre} style={{border: "solid red"}}>
+          <Item producto={producto[0]}></Item>  
+          <Item producto={producto[1]}></Item> 
+          <Item producto={producto[2]}></Item>   
+         
+          </div>
           </>    
     
         )
