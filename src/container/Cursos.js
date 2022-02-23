9a3d915c09d642a572774b18ebce416f3f699fdd
Candela Rivero <candelarivero01@gmail.com>
2022-02-23 11:32:41 -0500

    Cards

--- src/container/Cursos.js
+++ src/container/Cursos.js
@@ -155,7 +155,7 @@ const Cursos = ( {nombre}) => {
               </svg>
             </span>Get ready for the big leagues
           </p>
-          <button className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Button
+          <button className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Start
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
